3c7a3087db1fa8b6de67b80bc7260ef8bf0fbbdd
Elsa <edelmas@sempo10009.glm.lan>
2022-12-07 16:37:54 +0100

    on boucle directement dans le dossier urls au lieu de boucler sur les mois

--- src/index.js
+++ src/index.js
@@ -1,35 +1,45 @@
 import { promises as fs } from 'fs';
 import probe from 'probe-image-size';
 
-const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
-
-async function readFile(month, callback) {
+async function readFile(filename, callback) {
   try {
-    const data = await fs.readFile(`src/urls/${month.index}-${month.id}.tsv`);
+    const data = await fs.readFile(`src/urls/${filename}`);
     const urls = data.toString().split('\t')
 
-    callback(urls, month)
+    callback(urls, filename)
   } catch (error) {
     console.error(`Got an error trying to read the file: ${error.message}`);
   }
 }
 
-async function getRatios(urls, month) {
+async function getRatios(urls, filename) {
   let ratios = urls.map(async imgUrl => {
+    if (!imgUrl) return ''
     const imgInfos = await probe(imgUrl)
+    if (!imgInfos) return ''
     const ratio = (imgInfos.height / imgInfos.width).toFixed(3)
     return ratio
   })
-  ratios = await Promise.all(ratios)
-  console.log(month.id, '\n', ratios.join('\t'))
 
-  fs.writeFile(`src/ratios/${month.index}-${month.id}.tsv`, ratios.join('\t'), err => {
+  ratios = await Promise.all(ratios)
+  console.log(filename, '\n', ratios.join('\t'))
+
+  fs.writeFile(`src/ratios/${filename}`, ratios.join('\t'), err => {
     if (err) {
       console.error(err);
     }
   })
 }
 
-for (const [index, month] of months.entries()) {
-  readFile({ id: month, index: index + 1 }, getRatios)
-}
+(async () => {
+  try {
+    const files = await fs.readdir('src/urls');
+
+    for (const file of files) {
+      readFile(file, getRatios)
+    }
+  }
+  catch (e) {
+    console.error(e);
+  }
+})();
